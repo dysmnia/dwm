3d3a1372642760002d98ade33ee029d2d91ce22c
dysmnia <138967739+dysmnia@users.noreply.github.com>
2025-05-14 21:36:34 +0200

    fix some things

--- config.h
+++ config.h
@@ -73,6 +73,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_k,      setmfact,       {.f = +0.05} },
 	{ MODKEY,             		    XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_s,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
